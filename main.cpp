5a68924499bd368751d730dc4dd48d8fdfa46816
CabBerumen <a19100031@ceti.mx>
2024-02-17 22:01:20 -0600

    First commit bucle para turnos de cada ataque

--- main.cpp
+++ main.cpp
@@ -3,18 +3,43 @@
 #include "Enemy/Enemy.h"
 
 int main() {
-    Player *player = new Player("Victor", 100, 8, 4, 10);
-    Enemy *enemy = new Enemy("Estudiante de Redes", 50, 5, 2, 5, 10);
+    Player *player = new Player("Bocchi", 100, 8, 4, 10);
+    Enemy *enemy = new Enemy("Daemon", 100, 5,4 , 5, 10);
+    int round = 1;
 
     cout << player->toString() << endl;
+    cout << "---------------" << endl;
     cout << enemy->toString() << endl;
 
     cout << "=====================" << endl;
 
-    player->doAttack(enemy);
-    enemy->doAttack(player);
+    while (player -> getHealth() > 0 && enemy -> getHealth() > 0){
+        cout << "Round: " << round << ": " << endl;
+
+        player->doAttack(enemy);
+        cout << player->getName() << " ataco a: " << enemy->getName() << " || Salud restante de " << enemy->getName() << ": " << enemy->getHealth() << endl;
+
+        if (enemy-> getHealth() > 0){
+            enemy ->doAttack(player);
+            cout << enemy->getName() << " ataco a: " << player->getName() << " || Salud restante de " << player->getName() << ": " << player->getHealth() << endl;
+
+        }
+        round ++;
+    }
+
+    if (player->getHealth() > 0 && enemy -> getHealth() <= 0){
+        cout << player->getName() << " ha ganado " << endl;
+    } else if (enemy->getHealth() > 0 && player -> getHealth() <= 0){
+        cout << enemy->getName() << " ha ganado " << endl;
+    }
+
+
+    //player->doAttack(enemy);
+    //enemy->doAttack(player);
+    cout << "=====================" << endl;
 
     cout << player->toString() << endl;
+    cout << "---------------" << endl;
     cout << enemy->toString() << endl;
 
     delete player;
